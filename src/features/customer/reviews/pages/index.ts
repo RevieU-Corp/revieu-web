d21f98f8ade9d6ef0f790d237e0d46f48f263044
wayne5090 <liweijun0302@gmail.com>
2026-01-10 03:44:41 -0800

    fix(customer): remove dead exports in reviews pages

--- src/features/customer/reviews/pages/index.ts
+++ src/features/customer/reviews/pages/index.ts
@@ -1,4 +1,3 @@
+export { default as ReviewsPage } from './ReviewsPage';
 export { default as WriteReviewPage } from './WriteReviewPage';
-export { default as SimpleWriteReviewPage } from './SimpleWriteReviewPage';
 export { default as PostPage } from './PostPage';
-export { default as TestPage } from './TestPage';
